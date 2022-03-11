9995c7e9962ec2f5be04a30d2790359fa60de28e
Dr. Michael Krieger <michael.krieger@fau.de>
2022-03-11 21:21:59 +0100

    Update openBIS_load_dataset_filedata.py
    openBISGetDatasetFileIO is now a bit more tolerant with respect to the specification of path/filename via the parameter filename. It will open the first file in the dataset with a path/filename ending on the parameter filename.

--- openBIS_load_dataset_filedata.py
+++ openBIS_load_dataset_filedata.py
@@ -12,23 +12,31 @@ from urllib.request import urlopen # Datei-Download
 from io import StringIO # Heruntergeladene Daten als Datei für numpy.loadtxt zur Verfügung stellen
 
 # Funktion zum Laden von Dateien aus openBIS Dataset-Objekten
-# Parameter: permId = openBIS-PermId
+# Parameter: permId = openBIS-PermId, z. B. 20220307155803636-543
 #            filename = Dateiname der Datendatei im openBIS Dataset-Objekt (inkl. Dateiendung und ggf. Unterverzeichnis)
 #                       Beispiele: superdaten.txt, nur_das_beste/superdaten.txt
+#                       Es wird die erste Datei geladen, die auf filename endet
 # Rückgabe:  StringIO Dateiobjekt (virtuelles Dateiobjekt, das z. B. in numpy.loadtxt verwendet werden kann)
 def openBISGetDatasetFileIO(permId,filename):
     global o;
     try:
         # dataset von openBIS laden
         ds=o.get_dataset(permId)
-        # Dateiinhalt vom openBIS Datastore Server herunterladen
-        if (filename.find('/')>=0):
-            url=ds.file_links['original/'+filename]
+        # url zur Datei rausfinden
+        fileLinks=ds.file_links
+        url=''
+        for key in fileLinks.keys():
+            if (key.endswith(filename)):
+                url=fileLinks[key]
+                break
+        if (len(url)>0):
+            # Dateiinhalt vom openBIS Datastore Server herunterladen
+            filedata=urlopen(url).read()
+            filedata=filedata.decode('utf-8') # Bytedaten dekodieren (utf-8 wird angenommen)
+            fileIO=StringIO(filedata) # StringIO generiert ein virtuelles Dateiobjekt mit dem Inhalt filedata
         else:
-            url=ds.file_links['original/DEFAULT/'+filename]
-        filedata=urlopen(url).read()
-        filedata=filedata.decode('utf-8') # Bytedaten dekodieren (utf-8 wird angenommen)
-        fileIO=StringIO(filedata) # StringIO generiert ein virtuelles Dateiobjekt mit dem Inhalt filedata
+            # Die Datei gibt es nicht
+            fileIO=None
     except:
         # irgendetwas lief schief; wir geben dann None zurück
         fileIO=None
@@ -46,8 +54,11 @@ if (o.is_session_active()):
     print("Anmeldung bei openBIS erfolgreich")
     # Datei aus openBIS-Dataset-Objekt laden
     IO=openBISGetDatasetFileIO('your_permid','your_measurement_datafile.txt')
-    data=np.loadtxt(IO,skiprows=1)
-    print(data)
+    if (IO is not None):
+        data=np.loadtxt(IO,skiprows=1)
+        print(data)
+    else:
+        print("Datei wurde nicht gefunden")
     # Abmelden von openBIS
     o.logout()
 else:
