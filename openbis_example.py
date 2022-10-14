eeed9347704f7faa26b2f1df2c7d498631e2fdaf
Dr. Michael Krieger <michael.krieger@fau.de>
2022-10-14 11:56:35 +0200

    Update openbis_example.py
    example adapted to changes in openbis.py

--- openbis_example.py
+++ openbis_example.py
@@ -3,18 +3,18 @@ import matplotlib.pyplot as plt
 import openbis
 
 # Connect to openBIS
-o=openbis.login('https://my-openbis-url')
+openbis.login('https://my-openbis-url')
 
 # Example: load data from file in dataset object
-file=openbis.getDatasetFileIO(o,'myPermId','superdaten.txt')
+file=openbis.getDatasetFileIO('myPermId','superdaten.txt')
 data=np.loadtxt(file,skiprows=1).T
 print(data)
 plt.semilogx(data[0],data[1])
 
 # Example: load data from spreadsheet in experimental_step object
-data=openbis.getSpreadsheetData(o,'/myIdentifier')
+data=openbis.getSpreadsheetData('myPermId')
 print(data)
 plt.plot(data[0],data[1])
 
 # Disconnect
-openbis.logout(o)
+openbis.logout()
