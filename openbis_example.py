f80c70a46eff835cc676c3024c6aa3b71f072a90
Dr. Michael Krieger <michael.krieger@fau.de>
2023-02-16 19:28:47 +0100

    Update openbis_example.py

--- openbis_example.py
+++ openbis_example.py
@@ -3,16 +3,15 @@ import matplotlib.pyplot as plt
 import openbis
 
 # Connect to openBIS
-openbis.login('https://my-openbis-url')
+openbis.login('https://physics.openbis.data.fau.de')
 
 # Example: load data from file in dataset object
-file=openbis.getDatasetFileIO('myPermId','superdaten.txt')
-data=np.loadtxt(file,skiprows=1).T
+data=openbis.getDatasetData('20220315142624274-1716','tiefpass_bode2.txt',skiprows=1)
 print(data)
 plt.semilogx(data[0],data[1])
 
 # Example: load data from spreadsheet in experimental_step object
-data=openbis.getSpreadsheetData('myPermId')
+data=openbis.getSpreadsheetData('20220429111324303-4328',usecols=[0,1])
 print(data)
 plt.plot(data[0],data[1])
 
